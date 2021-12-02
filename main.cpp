442ebd81c17a55b2c7f36be8310ed86becb762c1
EthanC2 <ethanrcox@gmail.com>
2021-12-02 11:26:04 -0500

    ...

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "hashlib++/hashlibpp.h"
+
+int main()
+{
+    //MD5 hash generator
+    hashwrapper *myWrapper = new md5wrapper();
+    
+    try
+    {
+            std::cout << myWrapper->getHashFromString("Hello world") << std::endl;
+    }
+    catch(hlException&)
+    {
+            //your error handling here
+    }
+
+    //Cleanup
+    delete myWrapper;
+    myWrapper = NULL;
+
+    return 0;
+}
